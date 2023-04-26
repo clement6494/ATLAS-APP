f4e4ea0573bd3a06a8e23aef9fa2399e4dcf6829
clement <clement.conchezboueytou@edu.ece.fr>
2023-04-26 18:58:09 +0200

    feat: app

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,47 @@
+import {Suspense , useEffect , useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
+
+
+const Computers = () => {
+
+  const dumbbell = useGLTF('./3Dmodels/dumbbells/scene.gltf')
+
+  return (
+    <mesh>
+        <hemisphereLight intensity={1.55}
+        groundColor="black" />
+
+        <pointLight intensity={1} />
+
+        <primitive
+        object={dumbbell.scene}
+        scale={10}
+        
+        />
+
+
+    </mesh>
+  )
+}
+
+const ComputersCanvas = () => {
+  return (
+
+    <Canvas
+    frameloop='demand'
+    
+    camera={{ position: [20, 3, 5], fov: 50 }}
+    gl={{preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Computers />
+      </Suspense>
+    </Canvas>
+  )
+
+}
+export default ComputersCanvas
