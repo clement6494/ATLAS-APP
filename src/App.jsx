2c8319228072bf4580e83255bed348f3a2810a22
clement <clement.conchezboueytou@edu.ece.fr>
2023-05-10 17:35:12 +0200

    feat: canvas start

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,15 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 function App() {
   
 
+  
+
   return (
     <BrowserRouter> 
-      <div id='body' className='relative z-0 '>
-        <BackgroundScene className='fixed z-0 top-0 ' />
-        <div className='bg-hero-patternbg-cover bg-no-repeat bg-center '>
-          <Navbar />
-          <Hero></Hero>
-        </div>
-        
-        
-        <div className='relative z-0'>
-        <About />
-        <Programs />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
-          <Contact />
-          <StarsCanvas />
 
-        </div>
+      <div id='body' className='relative z-0 '>
         
+        <BackgroundScene/>
+      
       </div>
     </BrowserRouter>
   )
