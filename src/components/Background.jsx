53bc725542e6313bff747801d6ea44d08c0dbfd9
clement <clement.conchezboueytou@edu.ece.fr>
2023-05-10 17:35:30 +0200

    canvas start

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { motion } from 'framer-motion';
 
 import {styles} from './styles';
@@ -6,9 +6,39 @@ import { DumbbellCanvas } from './canvas';
 import { KettlebellCanvas } from './canvas';
 /**import { ComputerCanvas } from './canvas' ; */
 
-const Hero = () => {
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+
+
+
+
+
+const BackgroundScene = () => {
+
+  
+ 
+
   return (
-    
+    <div id='canvas-container' className='h-[100%]' > 
+    <Canvas>
+
+        <ambientLight intensity={0.1} />
+        <pointLight position={[10, 10, 10]} intensity={10} />
+
+        <mesh>
+          <torusGeometry args={ [ 2 , 1, 2, 70] } />
+          <meshNormalMaterial color='red'  />
+        </mesh>
+
+
+
+    </Canvas>
+   
+    </div>
+
+
+      /** 
       <div className= {`${styles.paddingX} absolute inset-0 top-[120px]
       max-w-7x1 mx-auto flex flex-col items-start gap-5 z-0`} >
 
@@ -19,10 +49,10 @@ const Hero = () => {
 
 
       </div>
-
+    */
       
     
   )
 }
 
-export default Hero
+export default BackgroundScene
