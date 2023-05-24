3254dbec77ed89e731144bc7490bbced10e134b8
clement <clement.conchezboueytou@edu.ece.fr>
2023-05-24 04:52:07 +0200

    feat: background objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import {Suspense,  useState, useEffect  } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, BackgroundScene, Programs, Footer } from './components';
-
-
 import CanvasLoader from './components/Loader';
+
+
+
+
+
+
 
 function App() {
   
@@ -23,6 +27,7 @@ function App() {
   }, []);
 
   
+
 
   return (
     <BrowserRouter> 
