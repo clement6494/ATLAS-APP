313d70994ff17d5c5fac4a161c8d910e061dfa51
clement <94296944+clement6494@users.noreply.github.com>
2024-07-10 15:45:51 +0200

    Update background color

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -180,7 +180,7 @@ const CameraAnimation = ({ section }) => {
 
   if (section==1)
   {return(<>
-          <color attach="background" args={['black']} />
+          <color attach="background" args={['#e5e5e5']} />
           <Wallpaper />
   </>
   )}
